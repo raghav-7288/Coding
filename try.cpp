2e9af67834e61b66cc1aa0c95e2c049fec1247e7
raghav-7288 <rgtm29@gmail.com>
2024-09-16 00:09:35 +0530

    add

--- try.cpp
+++ try.cpp
@@ -1,18 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
+int add(int a, int b)
+{
+    return a + b;
+}
+int add(int a, int b, int c)
+{
+    return a + b + c;
+}
 int main()
 {
 #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
 #endif
-    unordered_set<int> st;
-    st.insert(2);
-    st.insert(1);
-    st.insert(4);
-    st.insert(3);
-    for (auto a : st)
-        cout << a;
-
+    cout << add(2, 3);
+    cout << add(2, 3, 4);
     return 0;
 }
