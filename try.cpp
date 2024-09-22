3051f4578dc94cc724d03600e51eb503a9fe900d
raghav-7288 <rgtm29@gmail.com>
2024-09-22 23:54:26 +0530

    add

--- try.cpp
+++ try.cpp
@@ -1,20 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
-int add(int a, int b)
+int solve(vector<int> &a, vector<int> &b)
 {
-    return a + b;
-}
-int add(int a, int b, int c)
-{
-    return a + b + c;
+    int i = 0, j = 0, ans = 0, n = a.size();
+    while (i < n)
+    {
+        if (a[i] != b[j])
+        {
+            if (i < n - 1 && a[i + 1] == a[i])
+            {
+                swap(a[i + 1], b[j]);
+                ans += min(a[i + 1], b[j]);
+            }
+            else if (j < n - 1 && b[j + 1] == b[j])
+            {
+                swap(a[i], b[j + 1]);
+                ans += min(a[i], b[j + 1]);
+            }
+            else
+                return -1;
+        }
+        i++;
+        j++;
+    }
+    return ans;
 }
 int main()
 {
-#ifndef ONLINE_JUDGE
-    freopen("../input.txt", "r", stdin);
-    freopen("../output.txt", "w", stdout);
-#endif
-    cout << add(2, 3);
-    cout << add(2, 3, 4);
+    vector<int> A = {1, 1, 1, 1, 2, 2, 2, 5}, B = {3, 3, 3, 3, 4, 4, 5, 2};
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
+    cout << solve(A, B);
     return 0;
 }
