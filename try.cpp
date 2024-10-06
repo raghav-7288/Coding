0f4692752cd0140bbb2ebeb2f0a013261525cebd
raghav-7288 <rgtm29@gmail.com>
2024-10-06 19:15:47 +0530

    add

--- try.cpp
+++ try.cpp
@@ -2,16 +2,10 @@
 using namespace std;
 int main()
 {
-    priority_queue<pair<int, int>> pq;
-    pq.push({1, 2});
-    pq.push({1, 4});
-    pq.push({1, 3});
-    pq.push({2, 2});
-    while (!pq.empty())
-    {
-        cout << pq.top().first << " " << pq.top().second << endl;
-        pq.pop();
-    }
+#ifndef ONLINE_JUDGE
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+#endif
 
     return 0;
 }
