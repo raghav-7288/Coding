974f4c534fa7ba7f9c280fdab0b75f61eb9f8814
raghav-7288 <rgtm29@gmail.com>
2024-11-02 12:02:29 +0530

    add

--- MS/merge.cpp
+++ MS/merge.cpp
@@ -3,6 +3,30 @@ using namespace std;
 #define ll long long
 #define ld long double
 ll mod = 1e9 + 7;
+string solve(long long n)
+{
+    int p = 25;
+    string res = "";
+    while (n >= 1)
+    {
+        for (int i = p; i >= 0; i--)
+        {
+            int temp = 1 << p;
+            if (temp > n)
+            {
+                p--;
+            }
+            else
+            {
+                char ch = 'a' + p;
+                res = res + ch;
+                n = n - temp;
+                break;
+            }
+        }
+    }
+    return res;
+}
 int main()
 {
 #ifndef ONLINE_JUDGE
