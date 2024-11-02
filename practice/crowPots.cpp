20c9b8de8a3373670d5d963a02f33e7d969ecf0f
raghav-7288 <rgtm29@gmail.com>
2024-11-03 01:29:06 +0530

    add

--- practice/crowPots.cpp
+++ practice/crowPots.cpp
@@ -11,31 +11,81 @@ No of pots to overflow-- k
 Let say two pots are there with overflow no.s {5,58}, and crow has to overflow one pot(k=1).
 So crow will put 5 stones in pot with overflow no.(58), it will not overflow, then he will put in
 pot with overflow no.(5), hence the total no. of stones to make overflow one pot is=10.
+
+Example:
+Array of overflow no {5,58}
+Number of pots 2
+No of pots to overflow 1
+ans : 10
+
+Array of overflow no {65,69}
+Number of pots 2
+No of pots to overflow 1
+ans : 69
 */
 
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
 ll mod = 1e9 + 7;
+// int main()
+// {
+// #ifndef ONLINE_JUDGE
+//     freopen("../input.txt", "r", stdin);
+//     freopen("../output.txt", "w", stdout);
+// #endif
+//     int n, k, l = INFINITY, h;
+//     cin >> n >> k;
+//     vector<int> ofn(n), arr;
+//     for (auto &o : ofn)
+//         cin >> o;
+//     sort(ofn.begin(), ofn.end());
+//     for (int i = n - 1; i > 0; i--)
+//         ofn[i] = max(0, ofn[i] - ofn[i - 1]);
+//     int ans = 0;
+//     for (int i = 0; i < k; i++)
+//     {
+//         ans += ofn[i] * (n - i);
+//     }
+//     cout << ans;
+//     return 0;
+// }
+
 int main()
 {
 #ifndef ONLINE_JUDGE
     freopen("../input.txt", "r", stdin);
     freopen("../output.txt", "w", stdout);
 #endif
-    int n, k, l = INFINITY, h;
-    cin >> n >> k;
-    vector<int> ofn(n), arr;
-    for (auto &o : ofn)
-        cin >> o;
-    sort(ofn.begin(), ofn.end());
-    for (int i = n - 1; i > 0; i--)
-        ofn[i] = max(0, ofn[i] - ofn[i - 1]);
-    int ans = 0;
-    for (int i = 0; i < k; i++)
+    int n, k;
+    // Read the number of pots and number of pots to overflow
+    std::cin >> n >> k;
+    std::vector<int> O(n);
+    // Read the overflow numbers
+    for (int i = 0; i < n; ++i)
     {
-        ans += ofn[i] * (n - i);
+        std::cin >> O[i];
     }
-    cout << ans;
+    int stones_used = 0;
+    int pots_overflowed = 0;
+    // Create a copy of overflow numbers
+    std::vector<int> thresholds = O;
+    // Continue until K pots have overflowed
+    while (pots_overflowed < k)
+    {
+        for (int i = 0; i < n && pots_overflowed < k; ++i)
+        {
+            if (thresholds[i] > 0)
+            {
+                stones_used++;
+                thresholds[i]--;
+                if (thresholds[i] == 0)
+                {
+                    pots_overflowed++;
+                }
+            }
+        }
+    }
+    std::cout << stones_used << std::endl;
     return 0;
 }
