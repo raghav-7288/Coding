127c397e2757d867a97fb3dcdeb6d5b05891bbfc
raghav-7288 <rgtm29@gmail.com>
2024-11-02 10:59:51 +0530

    add

---
+++ MS/compression.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+ll mod = 1e9 + 7;
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("../../input.txt", "r", stdin);
+    freopen("../../output.txt", "w", stdout);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    return 0;
+}
