8dea8412902d979445b7243bdeb78d1f7c85a629
raghav-7288 <rgtm29@gmail.com>
2024-11-09 03:19:14 +0530

    add

--- try.cpp
+++ try.cpp
@@ -1,29 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define ld long double
-ll mod = 1e9 + 7;
+class Base
+{
+public:
+    virtual void Foo() { printf("Base::Foo()\n"); }
+};
+
+class Derived1 : public Base
+{
+public:
+    void Foo() override { printf("Derived1::Foo()\n"); }
+};
+
+class Derived2 : public Base
+{
+public:
+    void Foo() override { printf("Derived2::Foo()\n"); }
+};
+
+class Derived3 : public Derived1
+{
+public:
+    void Foo() { printf("Derived3::Foo()\n"); }
+};
+
+void callFoo(Base *base)
+{
+    Derived1 *derived1 = dynamic_cast<Derived1 *>(base);
+    Derived2 *derived2 = dynamic_cast<Derived2 *>(base);
+    Derived3 *derived3 = dynamic_cast<Derived3 *>(base);
+
+    if (base)
+    {
+        base->Foo();
+    }
+    if (derived1)
+    {
+        derived1->Foo();
+    }
+    if (derived2)
+    {
+        derived2->Foo();
+    }
+    if (derived3)
+    {
+        derived3->Foo();
+    }
+}
+
 int main()
 {
-    // #ifndef ONLINE_JUDGE
-    //     freopen("../../input.txt", "r", stdin);
-    //     freopen("../../output.txt", "w", stdout);
-    // #endif
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    char c = '1';
-    switch (c)
-    {
-    default:
-        cout << "def" << endl;
-        break;
-    case '4':
-        cout << 1 << endl;
-        break;
-    case '2':
-        cout << 1 << endl;
-        break;
-    }
-
+    Derived3 d;
+    callFoo(static_cast<Base *>(&d));
     return 0;
 }
