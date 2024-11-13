f109205013777f2345266061aae7632e664fcb34
raghav-7288 <rgtm29@gmail.com>
2024-11-13 20:15:56 +0530

    add

--- try.cpp
+++ try.cpp
@@ -3,14 +3,32 @@ using namespace std;
 #define ll long long
 #define ld long double
 ll mod = 1e9 + 7;
+int rev(int n)
+{
+    int res = 0;
+    while (n)
+    {
+        res = res * 10 + n % 10;
+        n /= 10;
+    }
+    return res;
+}
+bool ok(int i)
+{
+    int r = rev(i);
+    if ((int)(sqrt(i + r)) == sqrt(i + r))
+        return true;
+    return false;
+}
 int main()
 {
-    int x;
-    // taking integer input
-    scanf("Enter the two integers: %d", &x);
-
-    // printing integer output
-    printf("Printed using %%d: %d\n", x);
-    printf("Printed using %%i: %3i\n", x);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int ans = 0;
+    for (int i = 10; i < 100; i++)
+    {
+        if (ok(i))
+            cout << i << " ";
+    }
     return 0;
 }
