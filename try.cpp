b8a298bfb96af8d4fde9eab8974f9a428f21edec
raghav-7288 <rgtm29@gmail.com>
2024-11-20 02:13:05 +0530

    add

--- try.cpp
+++ try.cpp
@@ -1,70 +1,59 @@
-#include <bits/stdc++.h>
+// C++ program to demonstrate working
+// of Enum Classes
+
+#include <iostream>
 using namespace std;
-#define ll long long
-#define ld long double
-ll mod = 1e9 + 7;
-struct Node
-{
-    char data;
-    int freq;
-    Node *left, *right;
-    Node(char val, int f)
-    {
-        left = NULL;
-        right = NULL;
-        data = val;
-        freq = f;
-    }
-};
-struct Compare
-{
-    bool operator()(Node *l, Node *r)
-    {
-        return l->freq > r->freq;
-    }
-};
-void printHuffman(Node *top, string code)
-{
-    if (!top)
-        return;
-    if (top->data != '#')
-    {
-        cout << top->data << " : " << code << endl;
-    }
-    printHuffman(top->left, code + "0");
-    printHuffman(top->right, code + "1");
-}
-void huffman(map<char, int> &freq)
-{
-    priority_queue<Node *, vector<Node *>, Compare> minHeap;
-    for (auto m : freq)
-    {
-        minHeap.push(new Node(m.first, m.second));
-    }
-    while (minHeap.size() != 1)
-    {
-        Node *left = minHeap.top();
-        minHeap.pop();
-        Node *right = minHeap.top();
-        minHeap.pop();
-        Node *top = new Node('#', left->freq + right->freq);
-        top->left = left;
-        top->right = right;
-        minHeap.push(top);
-    }
-    printHuffman(minHeap.top(), "");
-}
+
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    map<char, int> freq;
-    freq['a'] = 5;
-    freq['b'] = 9;
-    freq['c'] = 12;
-    freq['d'] = 13;
-    freq['e'] = 16;
-    freq['f'] = 45;
-    huffman(freq);
+
+    enum class Color
+    {
+        Red,
+        Green,
+        Blue
+    };
+    enum class Color2
+    {
+        Red,
+        Black,
+        White
+    };
+    enum class People
+    {
+        Good,
+        Bad
+    };
+
+    // An enum value can now be used
+    // to create variables
+    int Green = 10;
+
+    // Instantiating the Enum Class
+    Color x = Color::Green;
+
+    // Comparison now is completely type-safe
+    if (x == Color::Red)
+        cout << "It's Red\n";
+    else
+        cout << "It's not Red\n";
+
+    People p = People::Good;
+
+    if (p == People::Bad)
+        cout << "Bad people\n";
+    else
+        cout << "Good people\n";
+
+    // gives an error
+    // if(x == p)
+    // cout<<"red is equal to good";
+
+    // won't work as there is no
+    // implicit conversion to int
+    // cout<< x;
+
+    cout << (int)x;
+
     return 0;
 }
