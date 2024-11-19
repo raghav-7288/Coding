79d39c04ce3e049653fe0b18ab2db0b6568f745f
raghav-7288 <rgtm29@gmail.com>
2024-11-19 20:10:44 +0530

    add

--- try.cpp
+++ try.cpp
@@ -3,26 +3,68 @@ using namespace std;
 #define ll long long
 #define ld long double
 ll mod = 1e9 + 7;
-class abc
+struct Node
 {
-private:
-    int x, y;
-
-public:
-    abc()
+    char data;
+    int freq;
+    Node *left, *right;
+    Node(char val, int f)
     {
-        x = 10;
-        y = 20;
-    }
-    void display()
-    {
-        cout << "x=" << x << " y=" << y;
+        left = NULL;
+        right = NULL;
+        data = val;
+        freq = f;
     }
 };
+struct Compare
+{
+    bool operator()(Node *l, Node *r)
+    {
+        return l->freq > r->freq;
+    }
+};
+void printHuffman(Node *top, string code)
+{
+    if (!top)
+        return;
+    if (top->data != '#')
+    {
+        cout << top->data << " : " << code << endl;
+    }
+    printHuffman(top->left, code + "0");
+    printHuffman(top->right, code + "1");
+}
+void huffman(map<char, int> &freq)
+{
+    priority_queue<Node *, vector<Node *>, Compare> minHeap;
+    for (auto m : freq)
+    {
+        minHeap.push(new Node(m.first, m.second));
+    }
+    while (minHeap.size() != 1)
+    {
+        Node *left = minHeap.top();
+        minHeap.pop();
+        Node *right = minHeap.top();
+        minHeap.pop();
+        Node *top = new Node('#', left->freq + right->freq);
+        top->left = left;
+        top->right = right;
+        minHeap.push(top);
+    }
+    printHuffman(minHeap.top(), "");
+}
 int main()
 {
-    // abc a;
-    // a.display();
-    cout << "Hi";
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    map<char, int> freq;
+    freq['a'] = 5;
+    freq['b'] = 9;
+    freq['c'] = 12;
+    freq['d'] = 13;
+    freq['e'] = 16;
+    freq['f'] = 45;
+    huffman(freq);
     return 0;
 }
