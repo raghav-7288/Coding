19deb5879c0024d6d539e0e2c415ad29ccf288c6
raghav-7288 <rgtm29@gmail.com>
2024-11-21 03:14:05 +0530

    add

--- try.cpp
+++ try.cpp
@@ -1,7 +1,30 @@
+// C++ Program to Demonstrate the working
+// of conversion operator
 #include <iostream>
+using namespace std;
+class Fraction
+{
+private:
+    int num, den;
+
+public:
+    Fraction(int n, int d)
+    {
+        num = n;
+        den = d;
+    }
+
+    // Conversion operator: return float value of fraction
+    operator float() const
+    {
+        return float(num) / float(den);
+    }
+};
+
 int main()
 {
-    int main = 10;
-    std::cout << main;
+    Fraction f(2, 5);
+    float val = f;
+    cout << val << '\n';
     return 0;
 }
